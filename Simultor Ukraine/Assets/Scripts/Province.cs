f39a8c64889c4e52e76ceadf7214e1e9c87bd8ee
Rach-1 <donin.yar@gmail.com>
2023-11-12 02:14:00 +0300

    Додав відображення назви провінції, тощо

--- Simultor Ukraine/Assets/Scripts/Province.cs
+++ Simultor Ukraine/Assets/Scripts/Province.cs
@@ -1,10 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Province : MonoBehaviour
 {
     public GameObject ProvMenu;
+    public string Name;
+    public string CountryTag;
+    public int Population;
+    public float Natality; //приріст населення
+
+    [SerializeField] Text name;
+    [SerializeField] Text population;
+    [SerializeField] Text natality;
 
     void Update()
     {
@@ -13,14 +20,23 @@ public class Province : MonoBehaviour
             Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
             Collider2D hitCollider = Physics2D.OverlapPoint(mousePosition);
+
             if (hitCollider != null && hitCollider.GetComponent<Province>() != null)
             {
-                ProvMenu.SetActive(true);
+                hitCollider.GetComponent<Province>().OpenMenu();
             }
             else
             {
                 ProvMenu.SetActive(false);
             }
         }
+    }
+    
+    public void OpenMenu()
+    {
+        ProvMenu.SetActive(true);
+        name.text = Name;
+        population.text = Population.ToString();
+        natality.text = Natality.ToString();
     }
 }
